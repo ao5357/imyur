5a0b564378851b9395952c786c605891a5a0b333
ao5357 <ao5357@gmail.com>
2012-03-07 18:20:45 -0500

    API progress

--- html/api/v1/index.php
+++ html/api/v1/index.php
@@ -1,1 +1,8 @@
-<?php phpinfo(); ?>
+<?php
+require_once 'AWSSDKforPHP/sdk.class.php';
+$dynamo = new AmazonDynamoDB();
+$table_name = 'addresses';
+
+/* Deal with input parameters */
+$input_url = trim($_GET['q']);
+echo $input_url;
