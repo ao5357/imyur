0bcfc1674e35bb29b5fb36626bb9a4f00e5997ed
ao5357 <ao5357@gmail.com>
2012-03-10 21:22:55 -0500

    Do I understand PHP objects?

--- html/redirect.php
+++ html/redirect.php
@@ -3,20 +3,20 @@ ini_set('display_errors', 1);
 $path = substr(trim($_GET['q']),1);
 $pos = strpos($path,'.');
 if($pos == 5 || $pos == 6){
-	echo "Condition for pos was true";
 	$hash = substr($path,0,$pos);
 	$from_apc = apc_fetch($hash,$apc_success);
 
 	if($apc_success){
-		header('Location: ' . $from_apc);
+		// header('Location: ' . $from_apc);
+		echo $from_apc;
 		}
 	else{
 		require_once 'AWSSDKforPHP/sdk.class.php';
 		$sdb = new AmazonSDB();
-		$response = $sdb->get_attributes('addresses',$hash);
+		$response = $sdb->get_attributes('addresses',$hash,'address');
 		$success = $response->isOK();
 		if($success){
-			print_r($response);
+			echo $response->body->GetAttributesResult->Attribute->Value;
 			//header('Location: ' . $address);
 			}
 		else{
