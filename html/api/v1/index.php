952e29af274e7c9e396ac3fb587e89dc58a5e413
ao5357 <ao5357@gmail.com>
2012-03-10 19:02:18 -0500

    base62encode from php itself

--- html/api/v1/index.php
+++ html/api/v1/index.php
@@ -11,17 +11,7 @@ $output = '';
 
 /* Functions */
 function base62encode($data){
-	$outstring = '';
-	$l = strlen($data);
-	for($i = 0;$i < $l;$i += 8){
-		$chunk = substr($data, $i, 8);
-		$outlen = ceil((strlen($chunk) * 8)/6); //8bit/char in, 6bits/char out, round up
-		$x = bin2hex($chunk); //gmp won't convert from binary, so go via hex
-		$w = gmp_strval(gmp_init(ltrim($x, '0'), 16), 62); //gmp doesn't like leading 0s
-		$pad = str_pad($w, $outlen, '0', STR_PAD_LEFT);
-		$outstring .= $pad;
-		}
-	return $outstring;
+	return gmp_strval(gmp_init($data,10),62);
 	}
 
 function counter(){
