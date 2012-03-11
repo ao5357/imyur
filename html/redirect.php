30490767cd97e09b778ed3102341dfb54d76f324
ao5357 <ao5357@gmail.com>
2012-03-10 20:55:11 -0500

    I no like this

--- html/redirect.php
+++ html/redirect.php
@@ -4,14 +4,16 @@ $path = substr(trim($_GET['q']),1);
 $pos = strpos($path,'.');
 if($pos == 5 || $pos == 6){
 	$hash = substr($path,0,$pos);
+	echo $hash;
 	$from_apc = apc_fetch($hash);
+	echo $from_apc;
 	if(!$from_apc){
 		require_once 'AWSSDKforPHP/sdk.class.php';
 		$sdb = new AmazonSDB();
 		$response = $sdb->get_attributes('addresses',$hash);
 		$success = $sdb->isOK();
 		if($success){
-			var_dump($response);
+			print_r($response);
 			//header('Location: ' . $address);
 			}
 		else{
