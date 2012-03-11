aa095cff74662302f48cdb8d575a900d1c25ca62
ao5357 <ao5357@gmail.com>
2012-03-10 21:01:00 -0500

    Am I tired?

--- html/redirect.php
+++ html/redirect.php
@@ -1,8 +1,9 @@
 <?php
 ini_set('display_errors', 1);
-$path = substr(trim($_GET['q']),1);
-$pos = strpos($path,'.');
+$path = substr(trim($_GET['q']),1); echo $path;
+$pos = strpos($path,'.'); echo $pos;
 if($pos == 5 || $pos == 6){
+	echo "Condition for pos was true";
 	$hash = substr($path,0,$pos);
 	echo $hash;
 	$from_apc = apc_fetch($hash);
@@ -11,13 +12,13 @@ if($pos == 5 || $pos == 6){
 		require_once 'AWSSDKforPHP/sdk.class.php';
 		$sdb = new AmazonSDB();
 		$response = $sdb->get_attributes('addresses',$hash);
-		$success = $sdb->isOK();
+		$success = $response->isOK();
 		if($success){
 			print_r($response);
 			//header('Location: ' . $address);
 			}
 		else{
-			header("HTTP/1.1 404 Not Found")
+			header("HTTP/1.0 404 Not Found")
 			}
 		}
 	else{
@@ -25,5 +26,5 @@ if($pos == 5 || $pos == 6){
 		}
 	}
 else{
-	header("HTTP/1.1 404 Not Found");
+	header("HTTP/1.0 404 Not Found");
 	}
